9558350291df4a8dbc29e87842108e5da143d49f
Ivo Petrov <1414vo@gmail.com>
2024-06-28 19:07:06 +0000

    Hyperparameter optimization documentation

--- scripts/optimize_hyperparams.py
+++ scripts/optimize_hyperparams.py
@@ -1,3 +1,16 @@
+"""A script for determining the optimal hyperparameters for a Gradient Boosting Classifier, Random Forest,
+and Support Vector Machine using grid search.
+
+Usage:
+
+.. code:: bash
+
+    $ python -m scripts.optimize_hyperparams <data_path> -o <output_path> --grids <grids_path>
+
+- *data_path*: The location of the data.
+- *output_path*: Where to store the outputs.
+- *grids_path*: The location of the json file detailing the grid nodes.
+"""
 from src.utils import k_fold_split_by_patient, optimize_hyperparams
 import pandas as pd
 import argparse
@@ -24,6 +37,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     data_path = args.data_path
     out_path = args.output
+
+    # Read input data
     try:
         data = pd.read_csv(f"{data_path}/Reduced_features.csv")
         patient_names = pd.read_csv(
@@ -34,6 +49,7 @@ if __name__ == "__main__":
         print(f"Could not find the data in {data_path}.")
         exit(1)
 
+    # Read grid json file
     try:
         with open(args.grids, "r") as f:
             grids = json.load(f)
@@ -41,6 +57,7 @@ if __name__ == "__main__":
         print(f"Could not find grid configuration file {args.cfg}.")
         exit(1)
 
+    # Split and optimize hyperparameters
     _, _, _, _, folds = k_fold_split_by_patient(data, models, patient_names)
     optimize_hyperparams(
         data, models, folds, grids, out_path=f"{args.output}/config_cv.json"
